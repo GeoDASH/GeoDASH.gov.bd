5a781493aec9e1be540da4238a96b557dfa7a00d
cjahangir <jahangir.cse09@gmail.com>
2017-02-06 19:57:42 +0600

    fixed data download

--- geonode/dashboard/views.py
+++ geonode/dashboard/views.py
@@ -2,7 +2,6 @@ import tempfile
 import os
 import tarfile
 import shutil
-import paramiko
 
 from urlparse import urlparse
 
@@ -12,54 +11,6 @@ from django.shortcuts import render
 
 from geonode import settings
 from forms import DataFolderBackupForm
-
-
-class SSHConnection(object):
-
-    def __init__(self, host, username, password, port=22):
-        """Initialize and setup connection"""
-        self.sftp = None
-        self.sftp_open = False
-
-        # open SSH Transport stream
-        self.transport = paramiko.Transport((host, port))
-
-        self.transport.connect(username=username, password=password)
-
-
-    def _openSFTPConnection(self):
-        """
-        Opens an SFTP connection if not already open
-        """
-        if not self.sftp_open:
-            self.sftp = paramiko.SFTPClient.from_transport(self.transport)
-            self.sftp_open = True
-
-
-    def get(self, remote_path, local_path=None):
-        """
-        Copies a file from the remote host to the local host.
-        """
-        self._openSFTPConnection()
-        self.sftp.get(remote_path, local_path, )
-
-
-    def put(self, local_path, remote_path=None):
-        """
-        Copies a file from the local host to the remote host
-        """
-        self._openSFTPConnection()
-        self.sftp.put(local_path, remote_path)
-
-
-    def close(self):
-        """
-        Close SFTP connection and ssh connection
-        """
-        if self.sftp_open:
-            self.sftp.close()
-            self.sftp_open = False
-        self.transport.close()
 
 
 def metadatabackup(request):
