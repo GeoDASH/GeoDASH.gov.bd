0edd36373616e38cba7dd93197ffc750babe1a62
RashedulAlam <rashedulalam.iit.du@gmail.com>
2018-04-17 12:19:32 +0600

    added tool tip on mouse hover on map

--- geonode/front_end/templates/Scripts/util/Tools/UI/measurementButtonsDirective.js
+++ geonode/front_end/templates/Scripts/util/Tools/UI/measurementButtonsDirective.js
@@ -7,7 +7,8 @@ return {
     restrict: 'EA',
     scope: {
         disableFunction : '=',
-        buttons : '='
+        buttons : '=',
+        content : '='
     },
     templateUrl: '/static/Templates/Tools/Map/measurementButtons.html',
     controller: [
@@ -19,10 +20,14 @@ return {
             $scope.toggleLineMeasurementTool=function () {
                 if($scope.disableFunction) $scope.disableFunction($scope.buttons["lineMeasurementTool"].toolsToDisable,mapTools,$scope.buttons);
                 $scope.buttons["lineMeasurementTool"].isActive= $scope.lineMeasurement();
+                if($scope.buttons["lineMeasurementTool"].isActive) $scope.content='Click to draw line';
+                else $scope.content='';
             };
             $scope.toggleAreaMeasurementTool=function () {
                 if($scope.disableFunction) $scope.disableFunction($scope.buttons["areaMeasurementTool"].toolsToDisable,mapTools,$scope.buttons);
                 $scope.buttons["areaMeasurementTool"].isActive=$scope.areaMeasurement();
+                if($scope.buttons["areaMeasurementTool"].isActive) $scope.content='Click to draw area';
+                else $scope.content='';
             };
         }
     ]
