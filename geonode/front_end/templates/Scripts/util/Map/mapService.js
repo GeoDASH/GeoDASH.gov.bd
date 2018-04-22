4b262c69c897479ac6d303941b80fcd6011d7e76
RashedulAlam <rashedulalam.iit.du@gmail.com>
2018-04-22 10:11:04 +0600

    fixed feature preview grid and map animation

--- geonode/front_end/templates/Scripts/util/Map/mapService.js
+++ geonode/front_end/templates/Scripts/util/Map/mapService.js
@@ -320,7 +320,7 @@
             //         });
             //     }
             // },
-            addDataLayer: function(layer) {
+            addDataLayer: function(layer,preventZoom) {
                 if (typeof layer.Name === 'undefined') {
                     return;
                 }
@@ -364,7 +364,7 @@
 
                 $q.all([p1, p2, p3])
                     .then(function() {
-                        map.addLayer(layer, false);
+                        map.addLayer(layer, preventZoom);
                     });
             },
             removeLayer: function(layerId) {
