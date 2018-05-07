c4c9484f4d083c57e5d62608b6f98c1735421c34
jahangir091 <jahangir.cse09@gmail.com>
2018-05-07 16:51:51 +0600

    added front_end settings file

---
+++ geonode/front_end_settings.py
@@ -0,0 +1,10 @@
+NSDI_FRONT_END_SETTINGS = {
+    'page_name': {
+        'button_name': False,
+        'button_name2':True,
+    },
+    'page_name2': {
+        'button_name1': False,
+        'button_name2': True
+    }
+}
