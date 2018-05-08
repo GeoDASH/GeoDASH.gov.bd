33702d0d13b459db0f3b9c80480d3032d5258af1
sharif018 <design_can_talk@yahoo.com>
2018-05-08 07:50:19 -0700

    front end settings applied to home page

--- geonode/front_end_settings.py
+++ geonode/front_end_settings.py
@@ -1,7 +1,24 @@
 NSDI_FRONT_END_SETTINGS = {
-    'page_name': {
-        'button_name': False,
+    'templates_base': {
+        'invite_user': False,
         'button_name2':True,
+    },
+    'page_name2': {
+        'button_name1': False,
+        'button_name2': True
+    }
+}
+
+
+GEODASH_FRONT_END_SETTINGS = {
+    'templates_base': {
+        'invite_user': False,
+        'backup_organization_layers':False,
+        'manage_sector_dept_section_createOrg': False,
+        'social_login':True,
+        'administration_header_menu':False,
+        'search_engine_title': 'GeoDASH',
+        'geodash_navbar_css': True
     },
     'page_name2': {
         'button_name1': False,
