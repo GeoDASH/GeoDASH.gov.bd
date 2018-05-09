2203f6590eeb775c9f6fe9f8b15e2cbcaed97750
sharif018 <design_can_talk@yahoo.com>
2018-05-09 02:36:28 -0700

    front end settings

--- geonode/front_end_settings.py
+++ geonode/front_end_settings.py
@@ -1,7 +1,18 @@
+
+
+from django.conf import settings
+
+
 NSDI_FRONT_END_SETTINGS = {
     'templates_base': {
         'invite_user': False,
-        'button_name2':True,
+        'backup_organization_layers':True,
+        'manage_sector_dept_section_createOrg': True,
+        'social_login':False,
+        'administration_header_menu':True,
+        'search_engine_title': 'NSDI',
+        'navbar_css': settings.STATIC_URL + 'geonode/css/navbar-nsdi.css?'
+
     },
     'page_name2': {
         'button_name1': False,
@@ -12,13 +23,14 @@ NSDI_FRONT_END_SETTINGS = {
 
 GEODASH_FRONT_END_SETTINGS = {
     'templates_base': {
-        'invite_user': False,
+        'invite_user': True,
         'backup_organization_layers':False,
         'manage_sector_dept_section_createOrg': False,
         'social_login':True,
         'administration_header_menu':False,
         'search_engine_title': 'GeoDASH',
-        'geodash_navbar_css': True
+        'navbar_css': settings.STATIC_URL + 'geonode/css/navbar-geodash.css?'
+
     },
     'page_name2': {
         'button_name1': False,
